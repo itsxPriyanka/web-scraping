4153f0bd30f097e742c44e0373fa4c07c9b96396
Priyanka <nameispriyanka05@gmail.com>
2023-06-24 14:52:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+from bs4 import BeautifulSoup
+import requests
+
+
+
+app= Flask(__name__)
+@app.route('/',methods=["GET","POST"])
+def index():
+    
+   url="https://www.businesstoday.in/latest/economy"
+   req=requests.get(url)
+   soup= BeautifulSoup(req.content,"html.parser")
+   outerdata=soup.find_all("div", class_="widget-listing" ,limit=7)
+   finalNews= ""
+   for data in outerdata:
+        news=data.div.div.a["title"]
+        finalNews += "\u2022 " + news + "\n"
+   return render_template("index.html",News=finalNews)
+
+
+
+
